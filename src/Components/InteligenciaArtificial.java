45015962903e844eb30b60f844075505b79b945d
matheus.marinelli <matheushlmarinelli@gmail.com>
2020-03-13 21:24:27 -0300

    classe do algoritmo da IA

---
+++ src/Components/InteligenciaArtificial.java
@@ -0,0 +1,19 @@
+package Components;
+
+/**
+ * Essa classe representa o algoritmo que será usado na busca do caminho
+ */
+public class InteligenciaArtificial {
+
+    /**
+     * Algoritmo que será rodado para encontrar a rota mais curta
+     * @param inicio vértice de início
+     * @param destino vértice de destino
+     * @return retorna a distância percorrida
+     */
+    public double nomeAlgoritmo(Vertice inicio, Vertice destino) {
+
+        return 0;
+    }
+
+}
